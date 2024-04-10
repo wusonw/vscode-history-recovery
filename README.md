fc104e0ea9259639bbf32041291a45d870964545
wangwuzhen <wzwang@nustarnuclear.com>
2024-04-10 20:10:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# VSR
+
+> a vscode timeline history file recovery cli tool
+
+## Installation
+
+```bash
+npm install -g vsr
+```
+
+```bash
+Usage: vsr [options] [command]
+
+A tool to recover deleted files from vscode folder history.
+
+Options:
+  -V, --version      output the version number
+  -h, --help         display help for command
+
+Commands:
+  list [options]     List all relative files from vscode folder history.
+  recover [options]  Recover files to special time.
+  help [command]     display help for command
+```
+
+## Attention
+
+- it only works on Windows platform
+- it only works for vscode
+- If you do not specify a working directory, the current execution path will be used.
+
+## LICENSE
+
+MIT
---
+++ index.js
@@ -0,0 +1,181 @@
+const { Command } = require("commander");
+const { terminal } = require("terminal-kit");
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+const { fileURLToPath } = require("url");
+
+const program = new Command();
+
+const isSubPath = (parentPath, childPath) => {
+  const relative = path.relative(parentPath, childPath);
+  const normalizedRelative = relative.replace(/[\\/]/g, "/").toLowerCase();
+  return (
+    normalizedRelative &&
+    !normalizedRelative.startsWith("..") &&
+    !path.isAbsolute(relative)
+  );
+};
+
+const formatTime = (time) => {
+  const date = new Date(time);
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+const handlefiles = (workspace) => {
+  const USERNAME = os.userInfo().username;
+  const historyPath = `C:/Users/${USERNAME}/AppData/Roaming/Code/User/History`;
+  const files = [];
+
+  const histories = fs.readdirSync(historyPath);
+  for (const historyFolder of histories) {
+    const entryJsonFilePath = path.join(
+      historyPath,
+      historyFolder,
+      "entries.json"
+    );
+    const entry = JSON.parse(fs.readFileSync(entryJsonFilePath, "utf8"));
+    try {
+      const resourcePath = fileURLToPath(entry.resource);
+      if (isSubPath(workspace, resourcePath)) {
+        files.push({
+          entry,
+          resourcePath,
+          historyPath,
+          historyFolder,
+        });
+      }
+    } catch (e) {}
+  }
+
+  return files;
+};
+
+const showList = (workspace) => {
+  const listHeader = ["File", "Updated At", "History Count"];
+  const files = handlefiles(workspace);
+  const list = files.map(({ entry, resourcePath }) => {
+    const { timestamp } = entry.entries.sort(
+      (a, b) => b.timestamp - a.timestamp
+    )[0];
+    return [
+      path.relative(workspace, resourcePath),
+      new Date(timestamp).toLocaleString(),
+      entry.entries.length,
+    ];
+  });
+  list.sort((a, b) => new Date(a[1]) - new Date(b[1]));
+  terminal.table([listHeader, ...list]);
+};
+
+const recover = (workspace) => {
+  const confirm = (inputTime) => {
+    try {
+      const time = new Date(inputTime);
+      if (isNaN(time.getTime())) {
+        terminal.error.nextLine(1).red("[ERROR]\t").grey("Invalid date.");
+        terminal.processExit(1);
+      } else {
+        terminal
+          .nextLine(1)
+          .yellow("Are you sure you want to go back to this time? [y/n] ");
+        terminal.yesOrNo(
+          { yes: ["y", "ENTER"], no: ["n"], echoYes: true, echoNo: true },
+          (_, res) => {
+            if (res) {
+              terminal.cyan("yes");
+              try {
+                const files = handlefiles(workspace);
+                files.forEach(
+                  ({ entry, resourcePath, historyPath, historyFolder }) => {
+                    const index = entry.entries.findIndex(
+                      (h) => h.timestamp >= time.getTime()
+                    );
+                    if (index >= 0) {
+                      const fileToRecover = path.join(
+                        historyPath,
+                        historyFolder,
+                        entry.entries[index].id
+                      );
+                      const fileToReplace = path.join(
+                        path.dirname(resourcePath),
+                        path
+                          .basename(resourcePath)
+                          .replace(
+                            /\.[^/.]+$/,
+                            `_recovery_${entry.entries[index].timestamp}` + "$&"
+                          )
+                      );
+                      fs.copyFileSync(fileToRecover, fileToReplace);
+                      terminal
+                        .nextLine(1)
+                        .green(`[RECOVERY]\t`)
+                        .grey(
+                          `${historyFolder}/${
+                            entry.entries[index].id
+                          } -> ${path.relative(workspace, fileToReplace)}`
+                        );
+                    }
+                  }
+                );
+                terminal
+                  .nextLine(1)
+                  .green(`[SUCCESS]\t`)
+                  .grey("Recovery done!");
+                terminal.processExit(0);
+              } catch (e) {
+                terminal.error.nextLine(1).red("[ERROR]\t").grey(e.message);
+                terminal.processExit(1);
+              }
+            } else {
+              terminal.cyan("no");
+              terminal.processExit(1);
+            }
+          }
+        );
+      }
+    } catch (e) {
+      terminal.error.nextLine(1).red("[ERROR]\t").grey(e.message);
+      terminal.processExit(1);
+    }
+  };
+  terminal.yellow("Which to go back to? (YYYY-MM-DD HH:mm:ss) ");
+  terminal.inputField(
+    {
+      cancelable: true,
+      default: formatTime(new Date()),
+    },
+    (_, input) => confirm(input)
+  );
+};
+
+program
+  .name("vsr")
+  .description("A tool to recover deleted files from vscode folder history.")
+  .version("1.0.0");
+
+program
+  .command("list")
+  .description("List all relative files from vscode folder history.")
+  .option("-w, --workspace <path>", "which folder to list")
+  .action((_, options) => {
+    const workspace = options.workspace
+      ? path.resolve(options.workspace)
+      : process.cwd();
+    showList(workspace);
+  });
+
+program
+  .command("recover")
+  .description("Recover files to special time.")
+  .option("-w, --workspace <path>", "which folder to recover")
+  .action((_, options) => {
+    const workspace = options.workspace
+      ? path.resolve(options.workspace)
+      : process.cwd();
+    recover(workspace);
+  });
+
+program.parse();
